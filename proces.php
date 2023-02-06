e48bcbf70a673b69f0496d58516ef4b7b6fca376
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2023-02-06 20:50:49 -0300

    pequenas mudanças

--- proces.php
+++ proces.php
@@ -1,8 +1,23 @@
 <?php
 
-function Mytest(){
-    $nome = $_POST['nome'];
-    echo $nome;
-}
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$cpf = $_POST['cpf'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
 
-Mytest();
+echo $nome;
+echo '<br>';
+echo $sobrenome;
+echo '<br>';
+echo $cpf;
+echo '<br>';
+echo $email;
+echo '<br>';
+echo $date;
+echo '<br>';
+echo $estado;
+echo '<br>';
+echo $cidade;
